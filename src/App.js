36d716bd681f959fd7a9cc39582d29f0787b690f
Christopher Nolan M. Barocio <christopher.nolanmb@gmail.com>
2024-10-17 23:48:14 -0600

    created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenido a mi aplicación React</h1>
+      <p>Esta app está desplegada en GitHub Pages.</p>
+    </div>
+  );
+}
+
+export default App;
